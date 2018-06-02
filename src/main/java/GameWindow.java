951094c5d1e27d24af69369a0ceeee176f5f23ce
Dylan Miao <dtm32@pitt.edu>
2018-06-02 19:13:29 -0400

    Added frame to GameWindow

---
+++ src/main/java/GameWindow.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.imageio.*;
+import java.io.*;
+
+public class GameWindow {
+    
+    public static void main(String[] args) {
+        System.out.println("GameWindow");
+        JFrame    _frame    = new JFrame("CS1630 Poker Game");
+        JButton[] _buttons  = new JButton[9];
+        JPanel    _ttt      = new JPanel();
+        JPanel    _newPanel = new JPanel();
+
+        new GameWindow();
+
+        _frame.setSize(400, 500);
+        _frame.setVisible(true);
+        _frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        _ttt.setLayout(new GridLayout(3, 3));
+        _newPanel.setLayout(new FlowLayout());
+        _frame.add(_ttt, BorderLayout.NORTH);
+        _frame.add(_newPanel, BorderLayout.SOUTH);
+    }
+
+}
