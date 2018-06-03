77790a5b80c38e17043362f9a7f2be6ac1d18604
Dylan Miao <dtm32@pitt.edu>
2018-06-02 20:28:35 -0400

    Added menu bar

--- src/main/java/GameWindow.java
+++ src/main/java/GameWindow.java
@@ -8,14 +8,20 @@ public class GameWindow {
     
     public static void main(String[] args) {
         System.out.println("GameWindow");
-        JFrame    _frame    = new JFrame("CS1630 Poker Game");
+        JFrame    _frame    = new JFrame("CS1530 Poker Game");
         JButton[] _buttons  = new JButton[9];
         JPanel    _ttt      = new JPanel();
         JPanel    _newPanel = new JPanel();
 
+        JMenuBar  _menuBar  = new JMenuBar();
+        JMenu     _menuFile = new JMenu("File");
+        JMenu     _menuGame = new JMenu("Game");
+
+        JMenuItem _newGame  = new JMenuItem("New Game");
+
         new GameWindow();
 
-        // _frame.setSize(400, 500);
+        _frame.setSize(800, 600);
         _frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         _frame.setVisible(true);
         _frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -23,6 +29,12 @@ public class GameWindow {
         _newPanel.setLayout(new FlowLayout());
         _frame.add(_ttt, BorderLayout.NORTH);
         _frame.add(_newPanel, BorderLayout.SOUTH);
+
+        _menuGame.add(_newGame);
+
+        _menuBar.add(_menuFile);
+        _menuBar.add(_menuGame);
+        _frame.setJMenuBar(_menuBar);
     }
 
 }
