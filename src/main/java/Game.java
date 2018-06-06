e9faaf2c24aa576e3be48e700201a6ae1433c668
djrowe79 <djr79@pitt.edu>
2018-06-06 01:23:33 -0400

    added player card functionality

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,27 +1,48 @@
 import java.awt.*;
 import javax.swing.*;
 import java.io.*;
+import java.util.*;
+import javax.imageio.*;
 
 public class Game extends JPanel {
 
     private final Color pokerGreen = new Color(71, 113, 72);
 
-    public Game() {	
+    public Game() {
+		int numOfPlayers = 7;
+		
 		Deck theDeck = new Deck();
 		Card[] communityCards = new Card[5];
-		String cards = "";
+		ArrayList<Card[]> playercards = new ArrayList<Card[]>();
+		String cards = "Community Cards: ";
+		String yourCards = "Your Cards: ";
 		for(int i=0; i<5; i++){
 			communityCards[i] = theDeck.draw();
 			cards += (communityCards[i].getName() + ", ");
 		}
-		JLabel _sampleLabel = new JLabel(cards);
 		
-        _sampleLabel.setBackground(pokerGreen);
+		for(int i=0; i<numOfPlayers; i++){
+			Card[] temp = new Card[5];
+			temp[0] = theDeck.draw();
+			temp[1] = theDeck.draw();
+			playercards.add(temp);
+			if(i == 0){
+				yourCards += (temp[0].getName() + ", " + temp[1].getName());
+			}
+		}
+		
+		JLabel _tableCards = new JLabel(cards);
+		JLabel _yourCards = new JLabel(yourCards);
+		
+        _tableCards.setBackground(pokerGreen);
+		_tableCards.setFont(new Font("Courier", Font.PLAIN, 48));
+		_yourCards.setFont(new Font("Courier", Font.PLAIN, 48));
 
         setBackground(pokerGreen);
         setVisible(true);
 
-        add(_sampleLabel);
+        add(_tableCards);
+		add(_yourCards);
     }
 
     public Game(File saveFile) {
