94b081ee4811f014e03b9e2e3629200b1f5e521c
djrowe79 <djr79@pitt.edu>
2018-06-18 22:45:06 -0400

    Have half of the possible hands determined

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,20 +1,32 @@
 import java.awt.*;
 
 public class Card{
-    private String name;
-    private Image  face;
-    
-    public Card(String name, Image face){
-        this.name = name;
-        this.face = face;
-    }
-    
-    public String getName(){
-        return this.name;
-    }
-    
-    public Image getFace(){
-        return this.face;
-    }
-        
+	private String name;
+	private Image  face;
+	private String value;
+	private String suit;
+	
+	public Card(String name, Image face){
+		this.name  = name.toUpperCase();
+		this.face  = face;
+		this.value = name.substring(0, name.length() - 1).toUpperCase();
+		this.suit  = name.substring(name.length() - 1).toUpperCase();
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public Image getFace(){
+		return this.face;
+	}
+
+	public String getValue(){
+		return this.value;
+	}
+
+	public String getSuit(){
+		return this.suit;
+	}
+		
 }
--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,6 +2,7 @@ import java.awt.*;
 import javax.swing.*;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import javax.imageio.*;
 import java.awt.event.*;
@@ -290,31 +291,374 @@ public class Game extends JPanel {
         return returnList;
     }
 
-    private void determineBestHand(){
+    private String determineBestHand(){
         //Determine which players to consider
         ArrayList<Player> remainingPlayers = new ArrayList<Player>();
+        ArrayList<Player> playersWithBestHand = new ArrayList<Player>();
+        int highestHighCard = 0;
+        String suit;
+
         for(Player p : players){
-            if(p.isActive()){
+            if(p.isPlayingHand()){
                 remainingPlayers.add(p);
             }
         }
 
         //Check if only one player remains
         if(remainingPlayers.size() == 1){
-            distributePot(remainingPlayers.get(0));
-            return;
+            distributePot(remainingPlayers);
+            return remainingPlayers.get(0).getName() + " has won as the last man Standing!";
+        }
+
+        for(Player p : remainingPlayers){
+            ArrayList<Card> allAvailableCards = new ArrayList<Card>();
+            allAvailableCards.addAll(Arrays.asList(tableCards));
+            allAvailableCards.addAll(Arrays.asList(p.getCards()));
+            p.setCards((Card[])allAvailableCards.toArray());
         }
 
         //Determine best hand
-    }
+        //Royal/Straight Flush?-------------------------------------------------------------------------
+        for(Player p : remainingPlayers){
+            suit = containsFlush(p.getCards());
+            int highCard = containsStraight(p.getCards());
+            if(!suit.equals("") && highCard >= highestHighCard){
+                if(highCard > highestHighCard){
+                   highestHighCard =  highCard;
+                   playersWithBestHand = new ArrayList<Player>();
+                }
 
-    private void distributePot(Player winner){
-        for(Player p : players){
-            if(p.equals(winner)){
-                p.adjustCash(pot);
-                pot = 0;
+                playersWithBestHand.add(p);
             }
         }
+
+        if(playersWithBestHand.size() > 0){
+            distributePot(playersWithBestHand);
+
+            StringBuilder resultStrBuilder = new StringBuilder();
+            String deliminator = "";
+            for(int i = 0; i < playersWithBestHand.size(); i++){
+                resultStrBuilder.append(deliminator)
+                                .append(playersWithBestHand.get(i).getName());
+                
+                deliminator = ", ";
+                if(i == playersWithBestHand.size() - 2){
+                    if(playersWithBestHand.size() == 2){
+                        deliminator = " and ";
+                    } else {
+                        deliminator = ", and ";
+                    }
+                }
+            }
+
+            if(playersWithBestHand.size() == 1){
+                resultStrBuilder.append(" won with ");
+            } else if(playersWithBestHand.size() == 2){
+                resultStrBuilder.append(" both won with ");
+            } else {
+                resultStrBuilder.append(" all won with ");
+            }
+
+            if(highestHighCard == 14){ //Ace
+                resultStrBuilder.append("a Royal Flush!!!");
+            } else if(highestHighCard == 13){ //King
+                resultStrBuilder.append("a King High Straight Flush!!");
+            } else if(highestHighCard == 12){ //Queen
+                resultStrBuilder.append("a Queen High Straight Flush!!");
+            } else if(highestHighCard == 11){ //Jack
+                resultStrBuilder.append("a Jack High Straight Flush!!");
+            } else {
+                resultStrBuilder.append("a " + highestHighCard + " High Straight Flush!!");
+            }
+
+            return resultStrBuilder.toString();
+        }
+
+        //Four of a kind?-------------------------------------------------------------------------------
+
+        //Full House?-----------------------------------------------------------------------------------
+
+        //Flush?----------------------------------------------------------------------------------------
+        highestHighCard = 0;
+        suit = "";
+        for(Player p : remainingPlayers){
+            suit = containsFlush(p.getCards());
+            int highCard = determineHighCard(p.getCards());
+            if(highCard > highestHighCard){
+                highestHighCard =  highCard;
+                playersWithBestHand = new ArrayList<Player>();
+            }
+
+            playersWithBestHand.add(p);
+        }
+
+        if(playersWithBestHand.size() > 0){
+            distributePot(playersWithBestHand);
+
+            StringBuilder resultStrBuilder = new StringBuilder();
+            String deliminator = "";
+            for(int i = 0; i < playersWithBestHand.size(); i++){
+                resultStrBuilder.append(deliminator)
+                                .append(playersWithBestHand.get(i).getName());
+                
+                deliminator = ", ";
+                if(i == playersWithBestHand.size() - 2){
+                    if(playersWithBestHand.size() == 2){
+                        deliminator = " and ";
+                    } else {
+                        deliminator = ", and ";
+                    }
+                }
+            }
+
+            if(playersWithBestHand.size() == 1){
+                resultStrBuilder.append(" won with ");
+            } else if(playersWithBestHand.size() == 2){
+                resultStrBuilder.append(" both won with ");
+            } else {
+                resultStrBuilder.append(" all won with ");
+            }
+
+            if(highestHighCard == 14){ //Ace
+                resultStrBuilder.append("an Ace High Flush!");
+            } else if(highestHighCard == 13){ //King
+                resultStrBuilder.append("a King High Flush!!");
+            } else if(highestHighCard == 12){ //Queen
+                resultStrBuilder.append("a Queen High Flush!!");
+            } else if(highestHighCard == 11){ //Jack
+                resultStrBuilder.append("a Jack High Flush!!");
+            } else {
+                resultStrBuilder.append("a " + highestHighCard + " High Flush!!");
+            }
+
+            return resultStrBuilder.toString();
+        }
+
+        //Straight?-------------------------------------------------------------------------------------
+        highestHighCard = 0;
+        for(Player p : remainingPlayers){
+            int highCard = containsStraight(p.getCards());
+            if(highCard >= highestHighCard){
+                if(highCard > highestHighCard){
+                   highestHighCard =  highCard;
+                   playersWithBestHand = new ArrayList<Player>();
+                }
+
+                playersWithBestHand.add(p);
+            }
+        }
+
+        if(playersWithBestHand.size() > 0){
+            distributePot(playersWithBestHand);
+
+            StringBuilder resultStrBuilder = new StringBuilder();
+            String deliminator = "";
+            for(int i = 0; i < playersWithBestHand.size(); i++){
+                resultStrBuilder.append(deliminator)
+                                .append(playersWithBestHand.get(i).getName());
+                
+                deliminator = ", ";
+                if(i == playersWithBestHand.size() - 2){
+                    if(playersWithBestHand.size() == 2){
+                        deliminator = " and ";
+                    } else {
+                        deliminator = ", and ";
+                    }
+                }
+            }
+
+            if(playersWithBestHand.size() == 1){
+                resultStrBuilder.append(" won with ");
+            } else if(playersWithBestHand.size() == 2){
+                resultStrBuilder.append(" both won with ");
+            } else {
+                resultStrBuilder.append(" all won with ");
+            }
+
+            if(highestHighCard == 14){ //Ace
+                resultStrBuilder.append("an Ace High Straight!!");
+            } else if(highestHighCard == 13){ //King
+                resultStrBuilder.append("a King High Straight!!");
+            } else if(highestHighCard == 12){ //Queen
+                resultStrBuilder.append("a Queen High Straight!!");
+            } else if(highestHighCard == 11){ //Jack
+                resultStrBuilder.append("a Jack High Straight!!");
+            } else {
+                resultStrBuilder.append("a " + highestHighCard + " High Straight!!");
+            }
+
+            return resultStrBuilder.toString();
+        }
+
+        //Three of a Kind?------------------------------------------------------------------------------
+
+        //Two Pair?-------------------------------------------------------------------------------------
+        
+        //One Pair?-------------------------------------------------------------------------------------
+
+        //High Card?------------------------------------------------------------------------------------
+        highestHighCard = 0;
+        for(Player p : remainingPlayers){
+            int highCard = determineHighCard(p.getCards());
+            if(highCard >= highestHighCard){
+                if(highCard > highestHighCard){
+                   highestHighCard =  highCard;
+                   playersWithBestHand = new ArrayList<Player>();
+                }
+
+                playersWithBestHand.add(p);
+            }
+        }
+
+        if(playersWithBestHand.size() > 0){
+            distributePot(playersWithBestHand);
+
+            StringBuilder resultStrBuilder = new StringBuilder();
+            String deliminator = "";
+            for(int i = 0; i < playersWithBestHand.size(); i++){
+                resultStrBuilder.append(deliminator)
+                                .append(playersWithBestHand.get(i).getName());
+                
+                deliminator = ", ";
+                if(i == playersWithBestHand.size() - 2){
+                    if(playersWithBestHand.size() == 2){
+                        deliminator = " and ";
+                    } else {
+                        deliminator = ", and ";
+                    }
+                }
+            }
+
+            if(playersWithBestHand.size() == 1){
+                resultStrBuilder.append(" won with ");
+            } else if(playersWithBestHand.size() == 2){
+                resultStrBuilder.append(" both won with ");
+            } else {
+                resultStrBuilder.append(" all won with ");
+            }
+
+            if(highestHighCard == 14){ //Ace
+                resultStrBuilder.append("an Ace High.");
+            } else if(highestHighCard == 13){ //King
+                resultStrBuilder.append("a King High.");
+            } else if(highestHighCard == 12){ //Queen
+                resultStrBuilder.append("a Queen High.");
+            } else if(highestHighCard == 11){ //Jack
+                resultStrBuilder.append("a Jack High.");
+            } else {
+                resultStrBuilder.append("a " + highestHighCard + " High.");
+            }
+
+            return resultStrBuilder.toString();
+        }
+        
+        return "No Winner!!"; //This shoud be impossible
+    }
+
+    private boolean containsCard(Card[] hand, String cardValue, String cardSuit){
+        for(Card card : hand){
+            if(card.getValue().equals(cardValue) && card.getSuit().equals(cardSuit)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int determineHighCard(Card[] hand){
+        int highCard = 0;
+        for(Card card : hand){
+            int val;
+            if(card.getValue().equals("A")){
+                val = 14;
+            } else if(card.getValue().equals("K")){
+                val = 13;
+            } else if(card.getValue().equals("Q")){
+                val = 12;
+            } else if(card.getValue().equals("J")){
+                val = 11;
+            } else {
+                val = Integer.parseInt(card.getValue());
+            }
+
+            if(val > highCard){
+                highCard = val;
+            }
+        }
+
+        return highCard;
+    }
+
+    //Determines if passed in array of cards contains a flush, returns suit if it does, empty string if not
+    private String containsFlush(Card[] hand){
+        //Clubs = 0, Diamonds = 1, Hearts = 2, Spades = 3
+        int[] suits = {0,0,0,0};
+        for(Card card : hand){
+            if(card.getSuit().equals("C")){
+                suits[0]++;
+            } else if(card.getSuit().equals("D")){
+                suits[1]++;
+            } else if(card.getSuit().equals("H")){
+                suits[2]++;
+            } else if(card.getSuit().equals("S")){
+                suits[3]++;
+            }
+        }
+
+        if(suits[0] >= 5){
+            return "C";
+        } else if(suits[1] >= 5){
+            return "D";
+        } else if(suits[2] >= 5){
+            return "H";
+        } else if(suits[3] >= 5){
+            return "S";
+        }
+        return "";
+    }
+
+    private int containsStraight(Card[] hand){
+        int[] values = new int[14];
+        int numInArow = 0;
+        int returnVal = -2;
+        for(Card card : hand){
+            if(card.getValue().equals("A")){
+                values[0] = 1;
+                values[13] = 1;
+            } else if(card.getValue().equals("K")){
+                values[12] = 1;
+            } else if(card.getValue().equals("Q")){
+                values[11] = 1;
+            } else if(card.getValue().equals("J")){
+                values[10] = 1;
+            } else {
+                values[Integer.parseInt(card.getValue()) - 1] = 1;
+            }
+        }
+
+        for(int i = 0; i < 14; i++){
+            if(values[i] == 1){
+                numInArow++;
+            } else {
+                numInArow = 0;
+            }
+
+            if(numInArow >= 5){
+                returnVal = i;
+            }
+        }
+        return returnVal + 1; //To offest the array starting at 0, returning a 10 means high card is 10, not jack
+    }
+
+    private void distributePot(ArrayList<Player> winners){
+        int winningsAmount = pot/winners.size();
+        for(Player p : players){
+            if(winners.contains(p)){
+                p.adjustCash(winningsAmount);
+            }
+        }
+
+        pot = 0;
+        return;
     }
     
 }
