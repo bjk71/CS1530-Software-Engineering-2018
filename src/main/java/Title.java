8c07718abefbb6e38b905a440f51a9f8d5006693
dtm32 <dtm32@pitt.edu>
2018-07-24 10:51:30 -0400

    Fixed checkstyle audit

--- src/main/java/Title.java
+++ src/main/java/Title.java
@@ -1,8 +1,14 @@
-import java.awt.*;
-import javax.swing.*;
+import java.awt.Color;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 
 public class Title extends JPanel {
 
+    /**
+     * Create generic Title JPanel object.
+     */
     public Title() {
         JLabel _sampleLabel = new JLabel("Title");
         
